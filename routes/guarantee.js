4fbb96e7f652c33aad91a2fe1692ee4a42a7964e
xc1124646582 <1124646582@qq.com>
2017-08-22 13:58:24 +0800

    8.22

--- routes/guarantee.js
+++ routes/guarantee.js
@@ -4,15 +4,16 @@ var pool=require("./../config.js");
 var router=express.Router();
 
 //业主发送 报修请求  
-//参数  name 邮包的名字     village  门牌号
+//参数     village  小区   con 报修问题    address门牌号
+//  village   address   从前台业主信息获取
 router.post('/yzguarantee',function(req,res){
 	res.header("Access-Control-Allow-Origin", "*");
-	var name=req.body["name"]
 	var con=req.body["con"]
 	var village=req.body["village"]  //小区
 	var address=req.body["address"]   //门牌号
-	pool.query(`insert into guarantee(name,con,village,address) values("${name}","${con}","${village}","${address}")`,function(err,rows){
-		pool.query('SELECT * from guarantee', function(err, rows, fields) {
+	console.log(village,address)
+	pool.query(`insert into guarantee(con,village,address) values("${con}","${village}","${address}")`,function(err,rows){
+		pool.query(`SELECT * from guarantee  where  address="${address}" and indexs=0 and village="${village}"`, function(err, rows, fields) {
 		if (err) throw err;
 	  	res.send(rows)
 	});
@@ -21,6 +22,30 @@ router.post('/yzguarantee',function(req,res){
 })
 
 
+
+//物业接收 报修请求  
+//参数     village  小区     
+router.post('/wyguarantee',function(req,res){
+	res.header("Access-Control-Allow-Origin", "*");
+	var village=req.body["village"]  //小区
+		pool.query(`SELECT * from guarantee  where  village="${village}" and indexs=0`, function(err, rows, fields) {
+		if (err) throw err;
+	  	res.send(rows)
+	});
+})
+
+
+
+//物业确认完成 报修请求  
+//参数     id  请求的id     
+router.post('/wyguarantees',function(req,res){
+	res.header("Access-Control-Allow-Origin", "*");
+	var id=req.body["id"]  
+	pool.query(`update guarantee set  indexs=1 where id=7`, function(err, rows, fields) {
+		if(err) throw err;
+		res.send(rows);
+	});
+})
 
 
 
