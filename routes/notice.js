443233036b55ac60a0c5f01df6966339a1fde83b
xc1124646582 <1124646582@qq.com>
2017-08-24 17:10:50 +0800

    8.24 a

--- routes/notice.js
+++ routes/notice.js
@@ -6,6 +6,23 @@ var router=express.Router();
 var fs=require('fs');   //重新命名
 var formidable=require('formidable');   //写入文件
 
+//！！物业展示通知
+//
+//参数  village 小区    
+router.post('/wytongzhis',function(req,res){
+	res.header("Access-Control-Allow-Origin", "*");
+	var village=req.body["village"]
+	var aa=[]
+	pool.query(`select * from notice where village="${village}"`,function(err,rows){
+		if(err) throw err;
+		for(var i in rows){
+			aa.unshift(rows[i])
+		}
+		res.send(aa);
+		})
+})
+
+
 
 //！！物业发送通知
 //
