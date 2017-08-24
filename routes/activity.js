5dff8e2cd77de7d51db019bc20080d29d428a2ea
xc1124646582 <1124646582@qq.com>
2017-08-24 20:15:00 +0800

    8.24 mds

--- routes/activity.js
+++ routes/activity.js
@@ -12,14 +12,17 @@ var formidable=require('formidable');   //写入文件
 //参数   name物业名字      village 小区      con 活动内容  title活动主题
 router.post('/wyhuodong',function(req,res){
 	res.header("Access-Control-Allow-Origin", "*");
+	var qs=[]
 	var village=req.body["village"]
 	var name=req.body["name"]
 	var con=req.body["con"]
-	var title=req.body["title"]
-	pool.query(`insert into activity(name,con,village,title) values("${name}","${con}","${village}","${title}")`,function(err,rows){
-	pool.query(`select * from activity`,function(err,rows){
+	pool.query(`insert into activity(name,con,village) values("${name}","${con}","${village}")`,function(err,rows){
+	pool.query(`select * from activity where village="${village}"`,function(err,rows){
 		if(err) throw err;
-		res.send(rows);
+		for(var i in rows){
+			qs.unshift(rows[i])
+		}
+		res.send(qs);
 	})
 		})
 })
@@ -30,10 +33,14 @@ router.post('/wyhuodong',function(req,res){
 //参数     village 小区  
 router.post('/wyhuodongs',function(req,res){
 	res.header("Access-Control-Allow-Origin", "*");
+	var qs=[]
 	var village=req.body["village"]
 	pool.query(`select * from activity where village="${village}"`,function(err,rows){
 		if(err) throw err;
-		res.send(rows);
+		for(var i in rows){
+			qs.unshift(rows[i])
+		}
+		res.send(qs);
 	})
 })
 
